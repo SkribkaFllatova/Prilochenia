7a55d86c7e887a7e6c4f9d7121713ca002b78fb6
Скрибка Влад и Филатова Юля <uliafilatova2003@gmail.com>
2025-02-15 09:13:45 +0700

    лб7

---
+++ lab6/app1.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+import os  # Импортируем модуль os
+
+app = Flask(__name__)
+
+INSTANCE_ID = os.getenv('INSTANCE_ID', 'unknown')
+
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify({"status": "healthy", "instance_id": INSTANCE_ID}), 200
+
+@app.route('/process', methods=['GET'])
+def process():
+    return jsonify({"message": "Processed by instance", "instance_id": INSTANCE_ID}), 200
+
+if __name__ == '__main__':
+    port = int(os.getenv('FLASK_RUN_PORT', 5002))
+    app.run(host='0.0.0.0', port=port)
